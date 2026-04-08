0e5a5a86f988e1ba1b49365b046a38a76c82291d
neliaa01 <neliaa01@users.noreply.github.com>
2026-04-08 15:50:02 +0900

    기능 테스트 코드

---
+++ lib/TEST/camera_test.h
@@ -0,0 +1,7 @@
+#ifndef CAMERA_TEST_H
+#define CAMERA_TEST_H
+
+void cameraSetup();
+void cameraLoop();
+
+#endif
---
+++ lib/TEST/performance_test.h
@@ -0,0 +1,7 @@
+#ifndef PERFORMANCE_TEST_H
+#define PERFORMANCE_TEST_H
+
+void performanceSetup();
+void performanceLoop();
+
+#endif
